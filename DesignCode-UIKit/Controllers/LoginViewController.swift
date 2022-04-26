e9905285eb79c4e41b0b299b0adbbacaf71277a7
AhmedMSoultan <ahmed.m.soultan@gmail.com>
2022-04-26 17:49:32 +0200

    Library screen added

--- DesignCode-UIKit/Controllers/LoginViewController.swift
+++ DesignCode-UIKit/Controllers/LoginViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import Combine
 
 class LoginViewController: UIViewController {
     
@@ -16,17 +18,83 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    enum LoginStatus {
+    case signIn
+    case signUp
+    }
+    
+    var emailIsEmpty = true
+    var passwordIsEmpty = true
+    private var tokens : Set<AnyCancellable> = []
+    var loginStatus : LoginStatus = .signUp {
+        didSet {
+            self.titleLabel.text = (loginStatus == .signUp) ? "Sign up" : "Sign in"
+            self.primaryBtn.setTitle((loginStatus == .signUp) ? "Create account" : "Sign in", for: .normal)
+            self.accessoryBtn.setTitle((loginStatus == .signUp) ? "Already have an account?" : "Don't have an account?", for: .normal)
+            self.passwordTextField.textContentType = (loginStatus == .signUp) ? .newPassword : .password
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.loginCard.alpha = 0
         UIView.animate(withDuration: 1.0, delay: 0.5, options: .curveEaseIn) {
             self.loginCard.alpha = 1
             self.loginCard.frame = self.loginCard.frame.offsetBy(dx: 0, dy: -300)
         }
 
+        emailTextField.publisher(for: \.text).sink { newValue in
+            self.emailIsEmpty = (newValue == "" || newValue == nil)
+        }.store(in: &tokens)
+        
+        passwordTextField.publisher(for: \.text).sink { newValue in
+            self.passwordIsEmpty = (newValue == "" || newValue == nil)
+        }.store(in: &tokens)
+        
+    }
+    
+    @IBAction func primaryBtnAction(_ sender: Any) {
+        
+        if (emailIsEmpty || passwordIsEmpty){
+            let alert = UIAlertController(title: "Missing information", message: "Please make sure to enter a valid email and password", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK ", style: .cancel, handler: nil))
+            self.present(alert, animated: true,completion: nil)
+        } else {
+            if(loginStatus == .signUp){
+                Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { authResult, error in
+                    guard error == nil else {
+                        print(error!)
+                        return
+                    }
+                    // Segue to the home screen
+                    self.goToHomeScreen()
+                }
+            }else{
+                Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { authResult, error in
+                    guard error == nil else {
+                        print(error!)
+                        return
+                    }
+                    // Segue to the home screen
+                    self.goToHomeScreen()
+                }
+            }
+            
+        }
         
         
+    }
+    
+    
+    @IBAction func accessoryBtnAction(_ sender: Any) {
+        self.loginStatus = (self.loginStatus == .signUp) ? .signIn : .signUp
+    }
+    
+    func goToHomeScreen() {
+        let homeVC = storyboard?.instantiateViewController(withIdentifier: "CustomTabBarController") as! CustomTabBarController
+        homeVC.modalTransitionStyle = .coverVertical
+        homeVC.modalPresentationStyle = .fullScreen
+        self.present(homeVC, animated: true, completion: nil)
     }
     
 }
